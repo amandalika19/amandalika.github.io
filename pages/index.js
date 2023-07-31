4305dbee2b332a209b4318e99c2ed9723dcb0fc9
Annapurna <amandalika19@outlook.com>
2023-07-30 23:56:58 -0700

    eslint fix

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,16 @@ import ArticleList from '../components/ArticleList'
 export default function Home() {
   return (
     <div><section class="landing-section" id="landing-section">
-    <div class="intro-div">
+      <div class="intro-div">
         <p class="welcome-para">Hi I'm Annapurna!</p>
         <p class="webdev-para">I am a <span>Student,</span> Coder,<span></span> and iced <span> vanilla </span> latte enthusiast.</p>
 
         <div class="hire-btn-div">
-            <button class="hire-btn">Hire Me!</button>
+          <button class="hire-btn">Hire Me!</button>
         </div>
 
-    </div>
-</section>
+      </div>
+    </section>
     </div>
   )
 }
